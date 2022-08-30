a04bd408156835fe29712167d597224ca7379426
nicola genesin <nicolagenesin@yahoo.it>
2022-08-30 22:46:16 +0200

    Fix domain name

--- env.ts
+++ env.ts
@@ -1,2 +1,2 @@
 export const dev = process.env.NODE_ENV === "development";
-export const url = dev ? "http://localhost:3000" : "https://longlistofcrypto.com";
+export const url = dev ? "http://localhost:3000" : "https://longlistofcrypto.xyz";
