a811ce5326e8f4e25d118a868343ec7c44d7e6d2
nicola genesin <nicolagenesin@yahoo.it>
2022-09-01 22:26:58 +0200

    Working version (needs styling)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,70 @@
 import React, { Fragment, useEffect, useState, useMemo } from 'react'
-import { Link, Box, Table, useMediaQuery, Tbody, Td, Th, Thead, Tr, Text, Select, HStack } from '@chakra-ui/react'
+import { Link, Box, useMediaQuery, Tbody, Td, Th, Thead, Tr, Text, Select, HStack, Input, InputGroup, InputLeftAddon } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { InView } from 'react-intersection-observer';
 import { url } from '../env'
+// import { FixedSizeList as List } from 'react-window';
+// import AutoSizer from "react-virtualized-auto-sizer";
+import { Table } from "react-fluid-table";
+
+
 
 const headers = ["#", "Project Name", "Category", "Sub-category", "Chain", "1-liner Description"]
+
+const rowStyle = (index: number) => ({
+  backgroundColor: index % 2 === 0 ? "#FEEBC8" : "#FFFAF0"
+});
+
+const columns = [
+  {
+    key: "index",
+    header: "#",
+    width: 50,
+    maxWidth: 50
+  },
+  {
+    key: "projectName",
+    header: "Project Name",
+    content: ({ row }) => <Box p={2} key="td-projectname" whiteSpace="nowrap" fontSize="xs" >🔗 <Link href={row.website}><b>{row.projectName}</b></Link></Box>,
+    width: 200,
+    maxWidth: 200
+  },
+  {
+    key: "category",
+    header: "Category",
+    content: ({ row }) => <Box p={2} key="td-projectname" whiteSpace="nowrap" fontSize="xs">{row.category}</Box>,
+    width: 200,
+    maxWidth: 200
+  },
+  {
+    key: "subcategory",
+    header: "Subcategory",
+    content: ({ row }) => <Box p={2} key="td-projectname" whiteSpace="nowrap" fontSize="xs">{row.subcategory}</Box>,
+    width: 200,
+    maxWidth: 200
+  },
+  {
+    key: "chain",
+    header: "Chain",
+    content: ({ row }) => <Box p={2} key="td-projectname" whiteSpace="nowrap" fontSize="xs">
+      {row.chain ? row.chain.split(', ').map((s: string, index: number) => (
+        <Fragment key={`fr-${index}`}>
+          {s}
+          <br />
+        </Fragment>
+      )) : "-"}
+    </Box>,
+    width: 150,
+    maxWidth: 150
+  },
+  {
+    key: "description",
+    header: "1-liner Description",
+    content: ({ row }) => <Box p={2} key="td-projectname" fontSize="xs">{row.description}</Box>,
+  },
+];
 
 const Home: NextPage = ({ results = [], lastAdded, lastUpdated }: any) => {
   const selectsData: any = useMemo(() => {
@@ -26,6 +85,7 @@ const Home: NextPage = ({ results = [], lastAdded, lastUpdated }: any) => {
     return tmp
   }, [results])
 
+  const [selectedWord, setSelectedWord] = useState<string>();
   const [selectedCategory, setSelectedCategory] = useState<string>();
   const [selectedSubCategory, setSelectedSubCategory] = useState<string | undefined>();
   const rows = useMemo(() => {
@@ -39,14 +99,32 @@ const Home: NextPage = ({ results = [], lastAdded, lastUpdated }: any) => {
       tmp = tmp.filter((row) => row[3] === selectedSubCategory)
     }
 
+    if (selectedWord) {
+      tmp = tmp.filter((row) => row[4].includes(selectedWord) || row[5].includes(selectedWord))
+    }
+
+    tmp = tmp.map(t => {
+      return {
+        index: t[0],
+        projectName: t[1],
+        category: t[2],
+        subcategory: t[3],
+        chain: t[4],
+        description: t[5],
+        website: t[6],
+      }
+    })
+
     return tmp
-  }, [results, selectedCategory, selectedSubCategory])
+  }, [results, selectedCategory, selectedSubCategory, selectedWord])
 
   useEffect(() => {
     setSelectedSubCategory(undefined)
   }, [selectedCategory]);
 
   const [isSmallViewport] = useMediaQuery('(min-width: 960px)')
+
+  console.log(rows)
 
   return (
     <Box className={styles.container} bg="#fffef2">
@@ -55,26 +133,25 @@ const Home: NextPage = ({ results = [], lastAdded, lastUpdated }: any) => {
         <meta name="description" content="A long list of blockchain/crypto/Web3 projects with respective 1-liner description." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <Box backgroundImage={"/header.jpg"} p={24} mb={4} rounded="xl" textAlign="center" >
-          <h1 className={styles.title}>
+        <Box backgroundImage={"/header.jpg"} p={8} mb={4} rounded="xl" textAlign="center" >
+          <Text as="h1" fontSize="4xl" mb={4}>
             <b>Long list of Crypto</b>
-          </h1>
+          </Text>
 
           <Text fontSize="xl" mt={4} mb={4}>
             🚧 <b>{results.length}</b> so far - {lastUpdated} - {lastAdded} 🚧
           </Text>
 
-          <Box mt="16px" mb="24px" >
+          <Box mt="16px" mb="8px" >
             <Text>A long list of blockchain/crypto/Web3 projects with respective 1-liner description.</Text>
             <Text>Managed by Michael, reach out on Twitter: <a style={{ color: 'teal' }} href={"https://twitter.com/brazenburrit0"}>brazenburrit0</a> or Telegram: @brazenburrito</Text>
-            <Text fontSize="xs" mt={2} fontStyle="italic">If you come across a new project or a project that has yet to be included in the list, please fill up this form and I will add to the list: <Link style={{ textDecoration: "underline" }} href="https://forms.gle/BssASs7NWRWdmQ4S9">Contribution Form Link</Link></Text>
+            <Text fontSize="xs" mt={4} fontStyle="italic">If you come across a new project or a project that has yet to be included in the list,<br />please fill up this form and I will add to the list: <Link style={{ textDecoration: "underline" }} href="https://forms.gle/BssASs7NWRWdmQ4S9">Contribution Form Link</Link></Text>
             <Text fontSize="xs" fontStyle="italic">Entirely optional but can support @ brazenburrito.eth | 0xb3e1EF38c290016dbfc3D13d1C91c32B6ec0C0C7</Text>
           </Box>
         </Box>
 
-        <HStack mb={8} mt={4}>
+        <HStack mb={8} mt={4} bg="#f1e5dc" p={4} rounded="md">
           <Select placeholder='Select Category' bg="white" size="sm" onChange={e => { setSelectedCategory(e.target.value) }} value={selectedCategory}>
             {Object.keys(selectsData).map(c => <option key={c} value={c}>{c}</option>)}
           </Select>
@@ -82,47 +159,22 @@ const Home: NextPage = ({ results = [], lastAdded, lastUpdated }: any) => {
           {selectedCategory && <Select placeholder='Select Sub Category' bg="white" size="sm" onChange={e => { setSelectedSubCategory(e.target.value) }} value={selectedSubCategory}>
             {selectsData[selectedCategory].map((c: string) => <option key={c} value={c}>{c}</option>)}
           </Select>}
-        </HStack>
 
-        <Box style={{
-          display: "block",
-          maxWidth: "100%",
-          overflowX: "auto",
-          overflowY: "hidden",
-          whiteSpace: !isSmallViewport ? "nowrap" : "normal"
-        }}>
-          <Table size='sm' variant='striped' colorScheme='orange'>
-            <Thead>
-              <Tr>
-                {headers.map((header, index) => (<Th key={`header-${index}`}>{header}</Th>))}
-              </Tr>
-            </Thead>
-            <Tbody>
-              {rows
-                .map((row: string[], index: number) => (
-                  <Tr key={`crypto-${index}`}>
-                    <Td p={2} key="td-index">{row[0]}</Td>
-                    <Td p={2} key="td-projectname" style={{ whiteSpace: "nowrap" }}>🔗 <Link href={row[6]}><b>{row[1]}</b></Link></Td>
-                    <Td p={2} key="td-category" fontSize="xs" style={{ whiteSpace: "nowrap" }}>{row[2]}</Td>
-                    <Td p={2} key="td-subcategory" fontSize="xs">{row[3]}</Td>
-                    <Td p={2} key="td-chain" fontSize="xs">
-                      {row[4] ? row[4].split(', ').map((s: string, index: number) => (
-                        <Fragment key={`fr-${index}`}>
-                          {s}
-                          <br />
-                        </Fragment>
-                      )) : "-"}
-                    </Td>
-                    <Td p={2} key="td-description" fontSize="xs">{row[5]}</Td>
-                  </Tr>
-                ))}
-            </Tbody>
-          </Table>
-        </Box>
+          <InputGroup size='sm' bg="white">
+            <InputLeftAddon children='Filter by text' />
+            <Input defaultValue={selectedWord} onChange={(e) => {
+              setSelectedWord(e.target.value)
+            }} />
+          </InputGroup>
+        </HStack>
+        <Table
+          data={rows}
+          columns={columns}
+          tableStyle={{}}
+          headerStyle={{}}
+          rowStyle={rowStyle}
+        />
       </main >
-      <footer className={styles.footer}>
-        <b>Contributed by Community; Owned by Community; For the Community</b>
-      </footer>
     </Box >
   )
 }
