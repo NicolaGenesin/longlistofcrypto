b3d07adebe4370daeb20f92555ad316a6bd72351
nicola genesin <nicolagenesin@yahoo.it>
2023-05-05 17:09:21 +0200

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const theme = extendTheme({
   fonts: {
@@ -10,9 +11,12 @@ const theme = extendTheme({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ChakraProvider theme={theme}>
-    <Component {...pageProps} />
-  </ChakraProvider>
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+      <Analytics />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
